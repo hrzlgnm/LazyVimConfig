07e133fa2a56223314a967d4ba81efb2e004df93
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-05-10 00:08:51 +0200

    feat(dap): add codelldb config for c++/c

---
+++ lua/plugins/dap-codelldb.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      dap.adapters.codelldb = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = "/home/valeni/.local/share/codelldb/extension/adapter/codelldb",
+          args = { "--port", "${port}" },
+        },
+      }
+      dap.configurations.cpp = {
+        {
+          name = "Launch file",
+          type = "codelldb",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+          end,
+          cwd = "${workspaceFolder}",
+          stopOnEntry = false,
+        },
+      }
+      dap.configurations.c = dap.configurations.cpp
+    end,
+  },
+}
