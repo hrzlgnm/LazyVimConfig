67d93bd12e28f2d7097a5c01d0ece1bf186c0efa
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-05-24 22:31:21 +0200

    Add custom auto cmd to mark certain conf files as dosini

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("mark_syntax_as_dosini"),
+  pattern = {
+    "diloconboard.conf",
+  },
+  callback = function()
+    vim.bo.filetype = "dosini"
+  end,
+})
