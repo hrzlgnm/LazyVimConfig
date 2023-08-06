87d8b1bbd6da75583ff4efa64b28e8ec95200859
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-08-06 23:21:02 +0200

    feat: cmake-tools generate without ninja on windoze

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,3 +1,7 @@
+local generate_options = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1", "-GNinja" }
+if vim.loop.os_uname().sysname == "Windows_NT" then
+  generate_options = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1" }
+end
 return {
   {
     "Civitasv/cmake-tools.nvim",
@@ -5,7 +9,7 @@ return {
     opts = {
       cmake_build_directory = "build",
       cmake_build_directory_prefix = "",
-      cmake_generate_options = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1", "-GNinja" },
+      cmake_generate_options = generate_options,
       cmake_always_use_terminal = true,
       cmake_dap_configuration = { name = "cpp", type = "codelldb", reqest = "launch" },
       cmake_compile_commands_from_lsp = true,
