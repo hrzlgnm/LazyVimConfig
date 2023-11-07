4a199d8b134a36763de083e55b28d94eb85e0b5b
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-11-07 21:01:57 +0100

    Add nvim-treesitter-refactor plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,34 @@ return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "nvim-treesitter/playground",
+    "nvim-treesitter/nvim-treesitter-refactor",
   },
   opts = {
+    refactor = {
+      highlight_definitions = {
+        enable = true,
+        clear_on_cursor_move = true,
+      },
+      highlight_current_scope = {
+        enable = true,
+      },
+      smart_rename = {
+        enable = true,
+        keymaps = {
+          smart_rename = "grr",
+        },
+      },
+      navigation = {
+        enable = true,
+        keymaps = {
+          goto_definition = "gnd",
+          list_definitions = "gnD",
+          list_definitions_toc = "gO",
+          goto_next_usage = "<a-*>",
+          goto_previous_usage = "<a-#>",
+        },
+      },
+    },
     ensure_installed = {
       "bash",
       "bitbake",
