99802a0f02da1b6f913a38ae05dd37c7f1493ef7
hrzlgnm <hrzlgnm@users.noreply.github.com>
2023-11-10 17:14:26 +0100

    cleanup cmake-tools config

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -7,13 +7,12 @@ return {
     "Civitasv/cmake-tools.nvim",
     event = "BufRead",
     cmd = { "CMakeBuild", "CMakeGenerate", "CMakeRun", "CMakeDebug" },
-    opts = {
-      cmake_build_directory = "build",
-      cmake_build_directory_prefix = "",
-      cmake_generate_options = generate_options,
-      cmake_always_use_terminal = true,
-      cmake_dap_configuration = { name = "cpp", type = "codelldb", reqest = "launch" },
-      cmake_compile_commands_from_lsp = true,
-    },
+    config = function()
+      require("cmake-tools").setup({
+        cmake_build_directory = "../build-${variant:buildType}",
+        cmake_generate_options = generate_options,
+        cmake_soft_link_compile_commands = true,
+      })
+    end,
   },
 }
