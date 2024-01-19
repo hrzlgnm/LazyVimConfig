5172f2105cc4d833e1bf401d6fbca3261de565b1
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-01-19 22:23:48 +0100

    Steal some interesting options from tjdevries
    https://github.com/tjdevries/config_manager/blob/master/xdg_config/nvim/plugin/options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,8 +3,46 @@
 -- Add any additional options here
 
 local opt = vim.opt
+opt.autoindent = true
+opt.cindent = true
+opt.wrap = true
+opt.relativenumber = true
+opt.number = true
 opt.shiftwidth = 4
 opt.tabstop = 4
+opt.softtabstop = 4
+opt.expandtab = true
+opt.breakindent = true
+opt.showbreak = "   "
+opt.linebreak = true
+
+opt.foldmethod = "marker"
+opt.foldlevel = 0
+opt.modelines = 1
+opt.belloff = "all"
+
+opt.joinspaces = true
+
+opt.fillchars = { eob = "~" }
+-- Ignore compiled files
+opt.wildignore = "__pycache__"
+opt.wildignore:append({ "*.o", "*~", "*.pyc", "*pycache*" })
+
+-- Floating window popup for completion on command line
+opt.pumblend = 17
+opt.wildmode = "longest:full"
+opt.wildoptions = "pum"
+
+opt.incsearch = true
+opt.showmatch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hidden = true
+opt.equalalways = true
+opt.splitright = true
+opt.splitbelow = true
+opt.updatetime = 1000
+opt.scrolloff = 10
 
 local g = vim.g
 g.loaded_perl_provider = 0
