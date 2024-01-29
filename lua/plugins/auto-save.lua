9544ac56cffc7eb94121de2b8955e002bcbcee8d
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-01-29 21:59:43 +0100

    set debounce delay to 1s for autosave, and only save when leaving insert mode

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,6 +1,9 @@
 return {
   "Pocco81/auto-save.nvim",
   config = function()
-    require("auto-save").setup({})
+    require("auto-save").setup({
+      debounce_delay = 1000,
+      trigger_events = { "InsertLeave" },
+    })
   end,
 }
