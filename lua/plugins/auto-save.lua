caa728b3468cdc8b70814016a7e9ff4dd1630d2b
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-02-12 15:55:38 +0100

    Remove auto-save again, as it hurts more than it brings benefits

--- lua/plugins/auto-save.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Pocco81/auto-save.nvim",
-  config = function()
-    require("auto-save").setup({
-      debounce_delay = 3000,
-      trigger_events = { "InsertLeave", "TextChanged" },
-    })
-  end,
-}
