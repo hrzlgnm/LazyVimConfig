8c8b538ac6b8b12457a26926ec6b24d5db8d7a14
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-04-11 01:35:56 +0200

    Tune mason for musl things

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,24 +1,35 @@
+local function get_opts()
+  local libc_is_musl = vim.fn.getfperm("/usr/lib/libc.so"):match("x") == "x"
+  local to_install = {
+    "bash-language-server",
+    "cmakelint",
+    "gersemi",
+    "groovy-language-server",
+    "hadolint",
+    "markdownlint",
+    "prettier",
+    "quick-lint-js",
+    "shellcheck",
+    "shellharden",
+    "shfmt",
+    "stylua",
+  }
+  local excluded = {}
+  if libc_is_musl then
+    table.insert(excluded, "clang-format")
+    table.insert(excluded, "codelldb")
+    table.insert(excluded, "clangd")
+  else
+    table.insert(to_install, "clang-format")
+    table.insert(to_install, "codelldb")
+  end
+  return { ensure_installed = to_install, automatic_installation = { exclude = excluded } }
+end
+
 return {
   "williamboman/mason.nvim",
   cmd = {
     "Mason",
   },
-  opts = {
-    ensure_installed = {
-      "bash-language-server",
-      "clang-format",
-      "cmakelint",
-      "codelldb",
-      "gersemi",
-      "groovy-language-server",
-      "hadolint",
-      "markdownlint",
-      "prettier",
-      "quick-lint-js",
-      "shellcheck",
-      "shellharden",
-      "shfmt",
-      "stylua",
-    },
-  },
+  opts = get_opts(),
 }
