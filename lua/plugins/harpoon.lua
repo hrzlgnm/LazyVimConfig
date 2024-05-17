64e6e51daf2c2c62e4c83433b89fd67648896428
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-05-17 16:40:36 +0200

    Harpoon tj

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,98 +1,22 @@
 return {
-  {
-    "ThePrimeagen/harpoon",
-    keys = {
-      {
-        "<leader>ha",
-        function()
-          require("harpoon.mark").add_file()
-        end,
-        desc = { "[H]arpoon [A]dd File" },
-      },
-      {
-        "<leader>ht",
-        function()
-          require("harpoon.ui").toggle_quick_menu()
-        end,
-        desc = { "[H]arpoon [T]oggle Quickmenu" },
-      },
-      {
-        "<M-1>",
-        function()
-          require("harpoon.ui").nav_file(1)
-        end,
-        desc = { "Harpoon jump to file 1" },
-      },
-      {
-        "<M-2>",
-        function()
-          require("harpoon.ui").nav_file(2)
-        end,
-        desc = { "Harpoon jump to file 2" },
-      },
-      {
-        "<M-3>",
-        function()
-          require("harpoon.ui").nav_file(3)
-        end,
-        desc = { "Harpoon jump to file 3" },
-      },
-      {
-        "<M-4>",
-        function()
-          require("harpoon.ui").nav_file(4)
-        end,
-        desc = { "Harpoon jump to file 4" },
-      },
-      {
-        "<M-5>",
-        function()
-          require("harpoon.ui").nav_file(5)
-        end,
-        desc = { "Harpoon jump to file 5" },
-      },
-      {
-        "<M-6>",
-        function()
-          require("harpoon.ui").nav_file(6)
-        end,
-        desc = { "Harpoon jump to file 6" },
-      },
-      {
-        "<M-7>",
-        function()
-          require("harpoon.ui").nav_file(7)
-        end,
-        desc = { "Harpoon jump to file 7" },
-      },
-      {
-        "<M-8>",
-        function()
-          require("harpoon.ui").nav_file(8)
-        end,
-        desc = { "Harpoon jump to file 8" },
-      },
-      {
-        "<M-9>",
-        function()
-          require("harpoon.ui").nav_file(9)
-        end,
-        desc = { "Harpoon jump to file 9" },
-      },
-      {
-        "<M-0>",
-        function()
-          require("harpoon.ui").nav_file(10)
-        end,
-        desc = { "Harpoon jump to file 10" },
-      },
-    },
-    opts = {},
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      require("telescope").load_extension("harpoon")
-    end,
-  },
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  config = function()
+    local harpoon = require "harpoon"
+    harpoon:setup()
+
+    vim.keymap.set("n", "<m-h><m-m>", function()
+      harpoon:list():add()
+    end)
+    vim.keymap.set("n", "<m-h><m-l>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    -- Set <space>1..<space>5 be my shortcuts to moving to the files
+    for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+      vim.keymap.set("n", string.format("<space>%d", idx), function()
+        harpoon:list():select(idx)
+      end)
+    end
+  end,
 }
