c575a4e69110298071d2b518b87ef312a03c46d9
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-07-16 00:35:43 +0200

    Map cmake sorting of blocks to meta-y

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -22,15 +22,15 @@ map("n", "YY", "va{Vy", { desc = "Select everything between {} including {}" })
 -- greatest remap ever
 map("x", "<leader>p", [["_dP]], { desc = "Paste selected text over current selection" })
 map("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Make it rain" })
+map(
+  "n",
+  "<m-y>",
+  [[<cmd>CMakeSelectFirstSortableRange<CR><cmd>'<,'>Sort iu<CR><Esc>]],
+  { desc = "Select and sort cmake sortable list range" }
+)
 
 local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
 -- vim way: ; goes to the direction you were moving.
 map({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
 map({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-
--- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-map({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-map({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-map({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-map({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
