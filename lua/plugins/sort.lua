219769eeec1f10ba5336a757b7d7dca3ba70986a
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-07-16 22:06:48 +0200

    chore: document why we reconfiure sort.nvim

--- lua/plugins/sort.lua
+++ lua/plugins/sort.lua
@@ -2,6 +2,9 @@ return {
   "sQVe/sort.nvim",
   config = function()
     require("sort").setup({
+      -- Default delimiters contain ':' which would cause
+      -- issues when sorting target_link_libraries in CMakeLists,
+      -- therefore we omit the ':' here.
       delimiters = {
         ",",
         "|",
