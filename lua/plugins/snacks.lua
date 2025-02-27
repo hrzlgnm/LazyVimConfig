5c2e08b06a3541db08b823c2180e6b8d92c90202
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-02-27 21:35:38 +0100

    Enable some snacky snacks

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    explorer = { enabled = true, replace_netrw = true },
+    dashboard = { enabled = true },
+    gitbrowse = { enabled = true },
+    image = {},
+    indent = {},
+    notifier = {},
+    scroll = { enabled = true },
+    terminal = { enabled = true },
+    wrods = { enanled = true },
+    zen = { enabled = true },
+  },
+}
