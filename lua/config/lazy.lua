41b10700089ab7463a632ef0c3238e39c4a0e8ad
Valentin Batz <vbatz@linetco.com>
2025-07-31 13:01:04 +0200

    Disalbe luarocks support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,10 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
+  rocks = {
+    -- Disable luarocks support alltogether
+    enabled = false,
+  },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = true, -- notify on update
