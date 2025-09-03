0d614c020611d31f9d6312b61560a50b1410591a
hrzlgnm <hrzlgnm@users.noreply.github.com>
2025-09-03 20:36:44 +0200

    Best ever backspace

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -79,3 +79,6 @@ map("v", ">", ">gv", { desc = "Indent right and reselect" })
 
 -- Better J behavior
 map("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+
+-- Best ever backspace
+map({ "c", "i" }, "<Backspace>", "<C-w>")
